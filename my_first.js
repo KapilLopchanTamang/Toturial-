8f31d8a4ebdf14460bac6001a9f8d6a5b2d0551b
Kapil Tamang <70058377+KapilLopchanTamang@users.noreply.github.com>
2023-10-20 21:10:02 +0545

    Create my_first.js

---
+++ my_first.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple JavaScript Example</title>
+</head>
+<body>
+    <h1>JavaScript Example</h1>
+    <p id="message"></p>
+
+    <script>
+        // JavaScript code
+        var message = "Hello, World!";
+        document.getElementById("message").textContent = message;
+    </script>
+</body>
+</html>
